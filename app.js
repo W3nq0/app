bfb59c4bc592d36a941037f483408deec5405d35
W3nq0 <165017532+W3nq0@users.noreply.github.com>
2024-03-27 15:37:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,44 +8,74 @@ const port = 3000;
 const excelFilePath = 'street.xlsx';
 const outputJsonFilePath = 'generated_addresses.json';
 
-function generateHouseOrApartmentNumber() {
-    const MIN_NUMBER = 1;
-    const MAX_NUMBER = 1000;
-    return Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
-}
-
 function parseExcelToJson(filePath) {
     const workbook = XLSX.readFile(filePath);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     const jsonData = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+    
     return jsonData;
+}
+
+function generateAddressesForStreet(streetName) {
+    const minnuber = 1;
+    const maxnumber = 100;
+    const streetAddresses = [];
+    let houseNumber = minnuber;
+    let apartmentNumber = minnuber;
+
+    for (let i = minnuber; i <= maxnumber; i++) {
+        for (let j = minnuber; j <= maxnumber; j++) {
+            const address = {
+                street: streetName,
+                houseNumber: houseNumber,
+                apartmentNumber: apartmentNumber
+            };
+
+            streetAddresses.push(address);
+            apartmentNumber++;
+
+            if (apartmentNumber > maxnumber) {
+                houseNumber++;
+                apartmentNumber = minnuber;
+            }
+        }
+    }
+
+    return streetAddresses;
 }
 
 function generateAddresses(streetCount) {
     const jsonData = parseExcelToJson(excelFilePath);
     console.log("Данные из Excel файла:", jsonData);
+
+    if (!Array.isArray(jsonData) || jsonData.length === 0) {
+        throw new Error('Отсутствуют данные в Excel файле.');
+    }
+
     if (streetCount > jsonData.length) {
         throw new Error(`Недостаточно данных. Файл содержит только ${jsonData.length} улиц.`);
     }
+
+    if (streetCount <= 0 || isNaN(streetCount)) {
+        throw new Error('Неверное количество улиц.');
+    }
+
     const selectedStreets = jsonData.slice(0, streetCount);
 
     selectedStreets.sort((a, b) => a.street.localeCompare(b.street));
 
     const groupedStreets = {};
+
     selectedStreets.forEach((data) => {
         const firstLetter = data.street[0].toLowerCase();
+
         if (!groupedStreets[firstLetter]) {
             groupedStreets[firstLetter] = [];
         }
-        const houseNumber = generateHouseOrApartmentNumber();
-        const apartmentNumber = generateHouseOrApartmentNumber();
-        const address = {
-            street: data.street,
-            houseNumber: houseNumber,
-            apartmentNumber: apartmentNumber
-        };
-        groupedStreets[firstLetter].push(address);
+
+        const streetAddresses = generateAddressesForStreet(data.street);
+        groupedStreets[firstLetter].push(...streetAddresses);
     });
 
     console.log("Группированные улицы:", groupedStreets);
@@ -57,17 +87,17 @@ function generateAddresses(streetCount) {
 }
 
 app.get('/api/cacheSetter', (req, res) => {
-    const streetCount = parseInt(req.query.streetCount);
+    try {
+        const streetCount = parseInt(req.query.streetCount);
 
-    if (!isNaN(streetCount) && streetCount > 0) {
-        try {
-            const streets = generateAddresses(streetCount);
-            res.json({ message: `Сгенерированы улицы для ${streetCount} улиц.`, streets });
-        } catch (error) {
-            res.status(400).json({ error: error.message });
+        if (isNaN(streetCount) || streetCount <= 0) {
+            throw new Error('Неверное количество улиц.');
         }
-    } else {
-        res.status(400).json({ error: 'Неверное количество улиц.' });
+        
+        const streets = generateAddresses(streetCount);
+        res.json({ message: `Сгенерированы улицы для ${streetCount} улиц.`, streets });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 });
 
